4fe6ed71e32adde1ff98392f7ac82a469607c704
Alberto Ceballos <alcebal2002@yahoo.es>
2017-02-20 13:02:35 +0100

    Included Spark and FreeMarker template

--- src/main/java/SparkMain.java
+++ src/main/java/SparkMain.java
@@ -1,18 +1,64 @@
 import static spark.Spark.get;
 import static spark.Spark.halt;
 
+import java.io.StringWriter;
 import java.sql.Timestamp;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.hazelcast.core.IMap;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import spark.Spark;
+
 public class SparkMain {
 
+	@SuppressWarnings("deprecation")
+	private static Configuration freemarkerConfig = new Configuration();
+
 	public static void main(String[] args) {
-        get("/", (req, res) -> "Welcome to Spark !");
+
+		freemarkerConfig.setClassForTemplateLoading(SparkMain.class,"/freemarker_templates/");
+		
+		get("/", (req, res) -> "Welcome to Spark !");
         get("/stop", (req, res) -> halt(401, "Go away!"));
-        get("/view", (req, res) -> "view");
+        get("/test_freemarker", (req, res) -> {
+    		 
+            StringWriter writer = new StringWriter();
+ 
+            try {
+                Template formTemplate = freemarkerConfig.getTemplate("form.ftl");
+ 
+                formTemplate.process(null, writer);
+            } catch (Exception e) {
+                Spark.halt(500);
+            }
+ 
+            return writer;
+        });
+        
+        Spark.post("/sait", (request, response) -> {
+            StringWriter writer = new StringWriter();
+ 
+            try {
+                String name = request.queryParams("name") != null ? request.queryParams("name") : "anonymous";
+                String email = request.queryParams("email") != null ? request.queryParams("email") : "unknown";
+ 
+                Template resultTemplate = freemarkerConfig.getTemplate("result.ftl");
+ 
+                Map<String, Object> map = new HashMap<>();
+                map.put("name", name);
+                map.put("email", email);
+ 
+                resultTemplate.process(map, writer);
+            } catch (Exception e) {
+                Spark.halt(500);
+            }
+ 
+            return writer;
+        });
         
         get("/monitor", (req, res) -> {
         	
